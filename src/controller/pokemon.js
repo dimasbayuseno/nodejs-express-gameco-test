d528b666361cc7347089ee514660c8381293dfc5
dimasbayuseno <dbswhnp@gmail.com>
2023-03-19 05:00:49 +0700

    add search by name

--- src/controller/pokemon.js
+++ src/controller/pokemon.js
@@ -6,12 +6,17 @@ const getAllPokemon = async (req, res) => {
     page = parseInt(req.query.page) || response.defaultPage
     per_page = parseInt(req.query.per_page) || response.defaultLimit
     url = 'https://pokeapi.co/api/v2/pokemon'
-    
+
+    if (req.query.name) {
+        url += `/${req.query.name}`
+    }
+
     url += `?limit=${per_page}&offset=${page}`
 
     try {
         const response = await axios.get(url);
-        res.json(response.data.results);
+        res.json(response.data);
+  
     } catch (error) {
         console.error(error);
         res.status(response.internalServerError).send(response.fetchError);

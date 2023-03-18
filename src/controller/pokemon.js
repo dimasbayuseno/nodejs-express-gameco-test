712f4530765b80d9eb5105ece20d88fb4b7ca967
dimasbayuseno <dbswhnp@gmail.com>
2023-03-19 05:35:21 +0700

    add sort

--- src/controller/pokemon.js
+++ src/controller/pokemon.js
@@ -5,16 +5,21 @@ const response = require("../const/const")
 const getAllPokemon = async (req, res) => {
     page = parseInt(req.query.page) || response.defaultPage
     per_page = parseInt(req.query.per_page) || response.defaultLimit
-    url = 'https://pokeapi.co/api/v2/pokemon'
+    url = 'https://pokeapi.co/api/v2'
 
-    if (req.query.name) {
-        url += `/${req.query.name}`
+    if (req.query.sort_by) {
+        url += `/${req.query.sort_by}` || "pokemon"
+    }
+
+    if (req.query.search_by) {
+        url += `/${req.query.search_by}`
     }
 
     url += `?limit=${per_page}&offset=${page}`
 
     try {
         const response = await axios.get(url);
+        console.log(response.data);
         res.json(response.data);
   
     } catch (error) {

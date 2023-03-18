97d9cf254ab4f675369b962dfbf6c7b793e52d4c
dimasbayuseno <dbswhnp@gmail.com>
2023-03-19 04:43:52 +0700

    add pagination

--- src/const/const.js
+++ src/const/const.js
@@ -10,11 +10,16 @@ const authorizedUsers = {
 const unauthorized = 401
 const internalServerError = 500
 
+const defaultPage = 1
+const defaultLimit = 10
+
 module.exports = {
     missingAuth,
     invalidAuth,
     unauthorized,
     authorizedUsers,
     internalServerError,
-    fetchError
+    fetchError,
+    defaultLimit,
+    defaultPage
 }
--- src/controller/pokemon.js
+++ src/controller/pokemon.js
@@ -3,8 +3,14 @@ const axios = require('axios');
 const response = require("../const/const")
 
 const getAllPokemon = async (req, res) => {
+    page = parseInt(req.query.page) || response.defaultPage
+    per_page = parseInt(req.query.per_page) || response.defaultLimit
+    url = 'https://pokeapi.co/api/v2/pokemon'
+    
+    url += `?limit=${per_page}&offset=${page}`
+
     try {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+        const response = await axios.get(url);
         res.json(response.data.results);
     } catch (error) {
         console.error(error);
